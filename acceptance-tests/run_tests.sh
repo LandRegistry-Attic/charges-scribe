770abeb0184c9a22ccbc2cb60d2f7bbb8c0dd83d
endamccormack <endamccormack0@gmail.com>
2015-06-26 15:30:22 +0100

    added -l shebang to use a loginshell to execute jenkins job

--- acceptance-tests/run_tests.sh
+++ acceptance-tests/run_tests.sh
@@ -4,6 +4,8 @@
 ### handy script.                                                                             ###
 #################################################################################################
 
+#!/bin/bash -l
+
 ### Retrieve the current location the script is running
 currentLocation="$(cd "$(dirname "$0")"; pwd)"
 
