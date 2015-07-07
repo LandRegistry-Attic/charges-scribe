c5f8cce2aea9e796c7714a1f61abe455f6d59c8b
Dawid Stelmaszek <d.stelmaszek@kainos.com>
2015-07-07 12:02:18 +0100

    sign deed endpoint changed

--- app/service/deed_api/interface.py
+++ app/service/deed_api/interface.py
@@ -2,5 +2,5 @@ class DeedApiInterface(object):
     def __init__(self, implementation):
         self.implementation = implementation
 
-    def can_sign(self, deed_id, borrower_id):
-        return self.implementation.can_sign(deed_id, borrower_id)
+    def sign(self, deed_id, borrower_id, signature):
+        return self.implementation.sign(deed_id, borrower_id, signature)
