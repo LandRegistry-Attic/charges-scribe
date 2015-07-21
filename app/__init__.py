bcc000db94b060065ac80bc52eb6718e45333384
Dawid Stelmaszek <d.stelmaszek@kainos.com>
2015-07-21 14:08:58 +0100

    helloworld restored

--- app/__init__.py
+++ app/__init__.py
@@ -2,14 +2,14 @@ from flask.ext.api import FlaskAPI
 from flask.ext.script import Manager
 from app.deed import sign
 from app.service.deed_api import make_deed_client
-from app import db, key
+from app import helloworld, db, key
 
 
 def create_manager(deed_api_client=make_deed_client):
     app = FlaskAPI(__name__)
     app.config.from_pyfile('config.py')
 
-    manager = Manager(app)
+    app.register_blueprint(helloworld.blueprint)
     app.register_blueprint(key.blueprint)
     app.register_blueprint(sign.blueprint(deed_api_client()))
 
